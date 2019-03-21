f67f3b3697f4b5664fc40edecde45e56b342bef0
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-03-21 09:38:52 +0100

    Lidt lækkert

---
+++ Photobook/Photobook/Models/CameraAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace Photobook.Models
+{
+    public interface ICameraAPI
+    {
+        void Open();
+
+    }
+
+    public class AndroidCamera : ICameraAPI
+    {
+        public void Open()
+        {
+            
+            
+        }
+    }
+}
