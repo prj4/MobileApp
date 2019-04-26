be1933a2722803b4343d97e434f9b7d1935bcc19
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-04-26 09:15:11 +0200

    Test i gang

---
+++ Photobook/UnitTest/FactoriesTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+using Photobook.Models;
+
+namespace UnitTest
+{
+    [TestFixture]
+    class FactoriesTest
+    {
+        [TestCase(DataType.Guest, typeof(GuestParser))]
+        public void TestGenerateParserFactory(DataType d, IJSONParser pars)
+        {
+            IJSONParser testPars = ParserFactory.Generate(d);
+
+            Assert.That(testPars.GetType(), Is.EqualTo(pars.GetType()));
+        }
+    }
+}
