977fc00993858c818c9ced3de8a6c2bcb8cc7b3c
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-05-20 09:39:29 +0200

    flyttede tilbage

---
+++ Photobook/UnitTest/PostfixTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Photobook.Models.ServerClasses;
+
+namespace UnitTest
+{
+    [TestFixture]
+    class PostfixTest
+    {
+        private IUrlPostfix uut;
+
+        [Test]
+        public void 
+    }
+}
